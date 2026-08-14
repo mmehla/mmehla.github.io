91a4e76fa0213dea1a47a50888787d47936963b7
Mukesh Mehla <mukesh.mehla@agnet.tamu.edu>
2026-08-14 15:04:53 -0500

    Update talk map locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Addressing Blue Water Scarcity Challenges in Banas River Basin Using Water Footprint Approach<br />Water-Energy-Food Nexus Symposium 2023, University of Twente; Enschede, Netherlands",
+    52.2209855,
+    6.8940537
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Smart Sensors for Enhancing Agricultural Water Conservation in Texas High Plains<br />2024 Artificial Intelligence in Agriculture Conference, Texas A&M University; College Station, Texas, USA",
+    30.6183939,
+    -96.3455991
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Crop Water Footprint of Major Crops in West Texas<br />Agricultural Water Sustainability Summit (AWSS); Lubbock, Texas, USA",
+    33.5855677,
+    -101.8470215
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Climate-smart Commodities: Sorghum-Cotton Rotation With and Without Cover Crops<br />Sorghum Field Day, TTU Research Farm; New Deal, Texas, USA",
+    33.7373052,
+    -101.836558
+  ],
+  [
+    "Enhancing Crop Water Use and Productivity in West Texas Agriculture: Insights from Two Decades of TAWC<br />ASA, CSSA, SSSA International Annual Meeting; San Antonio, Texas, USA",
+    29.4246002,
+    -98.4951405
+  ],
+  [
+    "Modeling Methane Dynamics in Agroecosystems Using Process-Based Models: A Model Comparison<br />SWAT Conference 2025 (Fall Meeting); Fort Collins, Colorado, USA",
+    40.5871782,
+    -105.0770113
+  ],
+  [
+    "Comparison of Contemporary Process-Based Methane (CH4) Modeling in Agroecosystem Models: A Review and Synthesis<br />AGU Fall Meeting 2025; New Orleans, Louisiana, USA",
+    29.9561422,
+    -90.0733934
+  ],
+  [
+    "Enhancing Carbon Cycling and Methane Dynamics in Agroecosystem Models<br />81st SWCS International Annual Conference; St. Louis, Missouri, USA",
+    38.6254063,
+    -90.190009
   ]
 ];
